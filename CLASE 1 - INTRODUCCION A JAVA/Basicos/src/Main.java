846664a00552d4c2c735c8c411989c71caa07ae0
hariatexe <ignacioariasfi@gmail.com>
2026-03-08 07:53:24 -0300

    Subiendo algunos cambios

--- CLASE 1 - INTRODUCCION A JAVA/Basicos/src/Main.java
+++ CLASE 1 - INTRODUCCION A JAVA/Basicos/src/Main.java
@@ -1,6 +1,44 @@
 import java.util.Scanner;
 public class Main {
+    public static void ProbandoCondicionales(){
+        int a = 10;
+        int b = 20;
+        if (a==10){
+            System.out.println("A es igual a 10");
+        }
+        // Como operador ternario
+        System.out.println("Probando el operador ternario: ");
+        System.out.println(a==b? "A es igual a B" : "A no es igual a B");
+        if (a == 10){
+            System.out.println("A es igual a 10");
+        } else if (a > 0) {
+            System.out.println("A es positivo");
+        } else {
+            System.out.println("A es negativo");
+        }
 
+    }
+    public static void ProbandoSwitch() {
+        System.out.println("Ingrese el valor para la variable A: ");
+        Scanner sc_1 = new Scanner(System.in);
+        int a = sc_1.nextInt();
+        System.out.println("Ingrese el valor para la variable B: ");
+        Scanner sc_2 = new Scanner(System.in);
+        int b = sc_2.nextInt();
+
+        switch (a+b) {
+            case 20:
+                System.out.println("Correcto el valor de la suma es 20");
+                break;
+            case 21:
+                System.out.println("Correcto el valor de la suma es 21");
+                break;
+            default:
+                System.out.println("Error, no es el valor deseado");
+        }
+        sc_1.close();
+        sc_2.close();
+    }
     public static void ProbandoFor() {
         int numero = 30;
         for ( int i = 10; i < numero; i++ ) {
@@ -17,24 +55,27 @@ public class Main {
             System.out.println("Valor del bucle: "+ i);
         }
     }
-    public static void ProbandoSwitch() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese un valor entero: ");
-        int numero = sc.nextInt();
-        switch(numero){
-            case 0:
-                System.out.println("El valor de numero es 0");
+    public static void ProbandoWhile() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println("El valor de i es: " + i++);
+            if (i > 5) {
+                continue;
+                // El ultimo numero que se imprime es el 5
+            }
+            if (i > 8) {
                 break;
-            case 2:
-                System.out.println("El valor de numero es 2");
-                break;
-            default:
-                System.out.println("Ese valor no se encontro en la db");
+            }
         }
-        sc.close();
+        /* En este caso el break nunca se ejecuta debido a que
+        * directamente para los mayores de 5 se corta con el continue
+        * y no se ejecuta abajo nada
+        * culo abierto
+        */
+
+
     }
     public static void main(String[] args) {
-        ProbandoSwitch();
-
+        ProbandoWhile();
     }
 }
